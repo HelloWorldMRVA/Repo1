762c9cee0533e432d70c6ff0964b212638436328
Nicolas Will <nicolaswill@github.com>
2024-06-20 15:40:59 -0400

    Update main.ts

--- .github/actions/src/main.ts
+++ .github/actions/src/main.ts
@@ -116,7 +116,7 @@ async function run(): Promise<void> {
 
     const dbZip = await download(dbUrl, language);
     const dbZipPath = path.resolve(dbZip);
-    console.log(dbZipPath);
+    console.log(`DB Zip Path: ${dbZipPath}`);
 
     console.log("Running query");
     /* ========== Run the analysis ========== */
